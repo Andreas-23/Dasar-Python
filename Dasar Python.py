10a588ef571561c5a1ca386f08f6fc025a48cb7d
Andreas-23 <andreas.sunbae@gmail.com>
2024-02-27 14:49:56 +0700

    First Commit

---
+++ Dasar Python.py
@@ -0,0 +1,7 @@
+print ("Hallo Para Mentor:D")
+
+nama = "Andreas M Simanullang"
+program = "Advance AI"
+
+print("Perkenalkan Nama Saya "+ nama + ", Dari Program " + program)
+print("Terimakasih HORASSS")
